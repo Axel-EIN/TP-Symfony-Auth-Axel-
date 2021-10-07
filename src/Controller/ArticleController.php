9debcb2e638a00004373be8af74c565a7cc0c638
axel <axel.ein@gmail.com>
2021-10-08 01:19:49 +0200

    Tentative de faire le post de commentaire

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleType;
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
 use App\Repository\ArticleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/article')]
 class ArticleController extends AbstractController
@@ -44,10 +46,24 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/{id}', name: 'article_show', methods: ['GET'])]
-    public function show(Article $article): Response
+    public function show(Request $request, Article $article, Commentaire $commentaire): Response
     {
+
+        // $commentaire = new Commentaire();
+        // $form = $this->createForm(CommentaireType::class, $commentaire);
+        // $form->handleRequest($request);
+
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     $entityManager = $this->getDoctrine()->getManager();
+        //     $entityManager->persist($commentaire);
+        //     $entityManager->flush();
+
+        //     return $this->redirectToRoute('article_show', [], Response::HTTP_SEE_OTHER);
+        // }
+
         return $this->render('article/show.html.twig', [
             'article' => $article,
+            // 'form' => $form,
         ]);
     }
 
---
+++ src/Form/CommentaireType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Commentaire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CommentaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('contenu', TextareaType::class)
+            ->add('datePublication', DateTimeType::class)
+            ->add('articleParent')
+            ->add('auteur', EntityType::class, [
+				'class' => Article::class,
+				'choice_label' => 'titre',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaire::class,
+        ]);
+    }
+}
