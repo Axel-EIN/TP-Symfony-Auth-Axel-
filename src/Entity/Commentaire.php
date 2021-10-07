fb7e30754596de55930505bcf5990254335f5cc5
axel <axel.ein@gmail.com>
2021-10-07 14:57:00 +0200

    Ajout des Relations des entités

--- src/Entity/Commentaire.php
+++ src/Entity/Commentaire.php
@@ -27,6 +27,18 @@ class Commentaire
      */
     private $datePublication;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Article::class, inversedBy="commentaires")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $articleParent;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Utilisateur::class, inversedBy="commentaires")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $auteur;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -52,6 +64,30 @@ class Commentaire
     public function setDatePublication(\DateTimeInterface $datePublication): self
     {
         $this->datePublication = $datePublication;
+
+        return $this;
+    }
+
+    public function getArticleParent(): ?Article
+    {
+        return $this->articleParent;
+    }
+
+    public function setArticleParent(?Article $articleParent): self
+    {
+        $this->articleParent = $articleParent;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?Utilisateur
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(?Utilisateur $auteur): self
+    {
+        $this->auteur = $auteur;
 
         return $this;
     }
